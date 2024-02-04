1f66e98b3b3049bd0112beb43372ac5f24fd7eb3
Leomttx <leonardo.t@escolar.ifrn.edu.br>
2024-02-04 20:55:42 -0300

    chore: add setup do shadcn-ui.✨

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
+// prettier.config.js
+module.exports = {
+    plugins: ['prettier-plugin-tailwindcss'],
+  }
+
 export default nextConfig;
