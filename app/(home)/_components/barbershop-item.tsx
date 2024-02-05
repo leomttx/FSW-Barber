49cc4780545b21925fa200080b9a37a81467c24d
Leomttx <leonardo.t@escolar.ifrn.edu.br>
2024-02-05 16:24:04 -0300

    feat: add componentes dos serviços das barbearias.✨

--- app/(home)/_components/barbershop-item.tsx
+++ app/(home)/_components/barbershop-item.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { Card, CardContent } from '@/app/_components/ui/card';
 import { Barbershop } from '@prisma/client';   
 import Image from 'next/image';
 import { Badge } from '@/app/_components/ui/badge';
 import { StarIcon } from 'lucide-react';
 import { Button } from '@/app/_components/ui/button';
+import { useRouter } from 'next/navigation';
 
 interface BarbershopItemProps {
     barbershop: Barbershop;
@@ -11,6 +14,15 @@ interface BarbershopItemProps {
 }
 
 const BrabershopItem = ({barbershop}: BarbershopItemProps) => {
+
+    // useRouter é um hook do Next.js que permite acessar o objeto router.
+    // O objeto router contém informações sobre a rota atual (como o caminho da URL, parâmetros de consulta) e permite a navegação programática para outras rotas.
+    const router = useRouter();
+
+    const handleBookingClick = () => { //Joga o usuário para a página de detalhes da barbearia
+        router.push(`/barbershops/${barbershop.id}`);
+    }
+
     return ( 
         <Card className=" min-w-[167px] max-w-[167px] rounded-2xl">
             <CardContent className="p-0 py-0">
@@ -39,7 +51,7 @@ const BrabershopItem = ({barbershop}: BarbershopItemProps) => {
                 <div className="px-3 pb-3">
                     <h2 className="font-bold overflow-hidden text-ellipsis text-nowrap">{barbershop.name}</h2>
                     <div className="text-sn text-gray-400 overflow-hidden text-ellipsis text-nowrap">{barbershop.address}</div>
-                    <Button className="w-full" variant="secondary">Reservar</Button>
+                    <Button className="w-full" variant="secondary" onClick={handleBookingClick}>Reservar</Button>
                 </div>
                 
             </CardContent>
