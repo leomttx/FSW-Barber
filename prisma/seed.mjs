d9e7b93ebc8cfbcc9aa7774f080efc25a7c42f0d
Leomttx <leonardo.t@escolar.ifrn.edu.br>
2024-02-05 13:24:29 -0300

    fix: seed ajustada.🐛🌱

--- prisma/seed.mjs
+++ prisma/seed.mjs
@@ -58,6 +58,52 @@ async function seedDatabase() {
       "Avenida Clássica, 707",
     ];
 
+    
+
+      const services = [
+      {
+        name: "Corte de Cabelo",
+        description: "Estilo personalizado com as últimas tendências.",
+        price: 60.0,
+        imageUrl:
+          "https://utfs.io/f/0ddfbd26-a424-43a0-aaf3-c3f1dc6be6d1-1kgxo7.png",
+      },
+      {
+        name: "Barba",
+        description: "Modelagem completa para destacar sua masculinidade.",
+        price: 40.0,
+        imageUrl:
+          "https://utfs.io/f/e6bdffb6-24a9-455b-aba3-903c2c2b5bde-1jo6tu.png",
+      },
+      {
+        name: "Pézinho",
+        description: "Acabamento perfeito para um visual renovado.",
+        price: 35.0,
+        imageUrl:
+          "https://utfs.io/f/8a457cda-f768-411d-a737-cdb23ca6b9b5-b3pegf.png",
+      },
+      {
+        name: "Sobrancelha",
+        description: "Expressão acentuada com modelagem precisa.",
+        price: 20.0,
+        imageUrl:
+          "https://utfs.io/f/2118f76e-89e4-43e6-87c9-8f157500c333-b0ps0b.png",
+      },
+      {
+        name: "Massagem",
+        description: "Relaxe com uma massagem revigorante.",
+        price: 50.0,
+        imageUrl:
+          "https://utfs.io/f/c4919193-a675-4c47-9f21-ebd86d1c8e6a-4oen2a.png",
+      },
+      {
+        name: "Hidratação",
+        description: "Hidratação profunda para cabelo e barba.",
+        price: 25.0,
+        imageUrl: "Fios hidratados, macios e brilhantes.",
+      },
+    ];
+
     // Criar 10 barbearias com nomes e endereços fictícios
     const barbershops = [];
     for (let i = 0; i < 10; i++) {
@@ -73,15 +119,7 @@ async function seedDatabase() {
         },
       });
 
-      // Adicionando alguns serviços para cada barbearia
-      for (let j = 0; j < 2; j++) {
-        const service = {
-          name: `Serviço ${j + 1} na ${name}`,
-          description: `Descrição do Serviço ${j + 1}`,
-          price: 30.0 + j * 10.0,
-          imageUrl: images[i + j + 1],
-        };
-
+      for (const service of services) {
         await prisma.services.create({
           data: {
             name: service.name,
@@ -99,6 +137,7 @@ async function seedDatabase() {
 
       barbershops.push(barbershop);
     }
+
 
     console.log("Seed completed!");
   } catch (error) {
