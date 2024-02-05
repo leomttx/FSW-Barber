b23cd3d52b61d7161e6146f64efd94faa12bdbc6
Leomttx <leonardo.t@escolar.ifrn.edu.br>
2024-02-04 22:25:02 -0300

    feat: add componente de agendamento(inicial).✨

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -3,12 +3,12 @@ import { ptBR } from "date-fns/locale";
 import Image from "next/image";
 import Header from "../_components/header";
 import Search from "./_components/search";
+import Bookingitem from "../_components/booking-item";
 
 export default function Home() {
   return (
     <div>
       <Header />
-
       <div className=" px-5 pt-5 ">
         <h2 className="text-xl font-bold ">Olá, Leo!</h2>
         <p className="capitalize text-sn ">
@@ -17,8 +17,14 @@ export default function Home() {
         })}
         </p>
       </div>
+
       <div className="px-5 mt-6">
         <Search />
+      </div>
+
+      <div className="px-5 mt-6">
+          <h2 className="mb-3 text-sm uppercase text-gray-400 font-bold">Agendamentos</h2>
+          <Bookingitem />
       </div>
 
     </div>
